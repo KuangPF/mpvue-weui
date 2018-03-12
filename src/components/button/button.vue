8adeef06253cab8bd9fed081a8583fa8d15a3bf0
kuangpengfei <1633397286@qq.com>
2018-03-12 23:30:43 +0800

    add testComponents button

---
+++ src/components/button/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="weui-btn" :type="type" :disabled="disabled" :plain="plain">{{btnMsg}}</button>
+</template>
+
+<script>
+export default {
+  name: 'mpvue-button',
+  props: {
+    type: {
+      type: String,
+      default: 'primary'
+    },
+    btnMsg: {
+      type: String,
+      default: '页面主操作 Normal'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    plain: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/componentsTest/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page">
+    <mpvue-button type="primary" btnMsg="自定义文字" plain></mpvue-button>
+  </div>
+</template>
+
+<script>
+import mpvueButton from '@/components/button/button';
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    mpvueButton
+  }
+}
+</script>
+
+<style>
+
+</style>
