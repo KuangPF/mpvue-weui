c126668e43f32749326d262c788e709787e264bc
kuangpengfei <1633397286@qq.com>
2018-03-28 22:56:22 +0800

    uploader count

--- src/pages/uploader/index.vue
+++ src/pages/uploader/index.vue
@@ -63,6 +63,7 @@ export default {
     chooseImage(e) {
       var _this = this;
       wx.chooseImage({
+        count: 1, // 默认9
         sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
         sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
         success: function (res) {
