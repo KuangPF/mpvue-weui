2081cba8d9e77ea1a40f67db92e8e4e605a87a90
kuangpengfei <1633397286@qq.com>
2018-04-15 23:42:04 +0800

    端口占用后自动更换端口 fixed [issues13](https://github.com/KuangPF/mpvue-weui/issues/13)

--- build/dev-server.js
+++ build/dev-server.js
@@ -10,6 +10,7 @@ var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
 var proxyMiddleware = require('http-proxy-middleware')
+var portfinder = require('portfinder')
 var webpackConfig = require('./webpack.dev.conf')
 
 // default port where dev server listens for incoming traffic
@@ -44,7 +45,9 @@ var compiler = webpack(webpackConfig)
 Object.keys(proxyTable).forEach(function (context) {
   var options = proxyTable[context]
   if (typeof options === 'string') {
-    options = { target: options }
+    options = {
+      target: options
+    }
   }
   app.use(proxyMiddleware(options.filter || context, options))
 })
@@ -63,7 +66,7 @@ app.use(require('connect-history-api-fallback')())
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+// var uri = 'http://localhost:' + port
 
 var _resolve
 var readyPromise = new Promise(resolve => {
@@ -80,17 +83,26 @@ var readyPromise = new Promise(resolve => {
 //   _resolve()
 // })
 
-var server = app.listen(port, 'localhost')
-
-// for 小程序的文件保存机制
-require('webpack-dev-middleware-hard-disk')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  quiet: true
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = port
+  portfinder.getPortPromise()
+    .then(newPort => {
+      if (port !== newPort) {
+        console.log(`${port}端口被占用，开启新端口${newPort}`)
+      }
+      var server = app.listen(newPort, 'localhost')
+      // for 小程序的文件保存机制
+      require('webpack-dev-middleware-hard-disk')(compiler, {
+        publicPath: webpackConfig.output.publicPath,
+        quiet: true
+      })
+      resolve({
+        ready: readyPromise,
+        close: () => {
+          server.close()
+        }
+      })
+    }).catch(error => {
+      console.log('没有找到空闲端口，请打开任务管理器杀死进程端口再试', error)
+    })
 })
-
-module.exports = {
-  ready: readyPromise,
-  close: () => {
-    server.close()
-  }
-}
