23e68f01edc33e3b1a3bddcbea432f23f151aa10
KuangPF <1633397286@qq.com>
2019-10-04 10:03:27 +0800

    mod: button style

--- src/pages/button/index.vue
+++ src/pages/button/index.vue
@@ -1,31 +1,23 @@
 <template>
   <div class="page">
-    <div class="page">
-      <div class="page__hd">
-        <div class="page__title">Button</div>
-        <div class="page__desc">按钮，WeUI采用小程序原生的按钮为主体，加入一些间距的样式。</div>
-      </div>
-      <div class="page__bd page__bd_spacing">
-        <button class="weui-btn" type="primary">页面主操作 Normal</button>
-        <button class="weui-btn" type="primary" disabled="true">页面主操作 Disabled</button>
+    <div class="page__hd">
+      <div class="page__title">Button</div>
+      <div class="page__desc">按钮，WeUI采用小程序原生的按钮为主体，加入一些间距的样式。</div>
+    </div>
+    <div class="page__bd page__bd_spacing">
+      <button class="weui-btn" type="primary">页面主操作</button>
+      <button class="weui-btn" type="primary" disabled="true">页面主操作</button>
 
-        <button class="weui-btn" type="default">页面次要操作 Normal</button>
-        <button class="weui-btn" type="default" disabled="true">页面次要操作 Disabled</button>
+      <button class="weui-btn" type="default">页面次要操作</button>
+      <button class="weui-btn" type="default" disabled="true">页面次要操作</button>
 
-        <button class="weui-btn" type="warn">警告类操作 Normal</button>
-        <button class="weui-btn" type="warn" disabled="true">警告类操作 Disabled</button>
+      <button class="weui-btn" type="warn">警告类操作</button>
+      <button class="weui-btn" type="warn" disabled="true">警告类操作</button>
 
-        <div class="button-sp-area">
-          <button class="weui-btn" type="primary" plain="true">按钮</button>
-          <button class="weui-btn" type="primary" disabled="true" plain="true">按钮</button>
-
-          <button class="weui-btn" type="default" plain="true">按钮</button>
-          <button class="weui-btn" type="default" disabled="true" plain="true">按钮</button>
-
-          <button class="weui-btn mini-btn" type="primary" size="mini">按钮</button>
-          <button class="weui-btn mini-btn" type="default" size="mini">按钮</button>
-          <button class="weui-btn mini-btn" type="warn" size="mini">按钮</button>
-        </div>
+      <div class="button-sp-area">
+        <button class="weui-btn mini-btn" type="primary" size="mini">按钮</button>
+        <button class="weui-btn mini-btn" type="default" size="mini">按钮</button>
+        <button class="weui-btn mini-btn" type="warn" size="mini">按钮</button>
       </div>
     </div>
   </div>
@@ -42,6 +34,13 @@ export default {
 }
 
 </script>
-<style>
-
+<style scoped>
+.button-sp-area{
+    margin: 0 auto;
+    padding-top: 15px;
+    text-align:center;
+}
+.mini-btn{
+    margin: 0 4px;
+}
 </style>
